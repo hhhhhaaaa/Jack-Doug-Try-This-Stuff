2783abc215eb8359e76db3197400b8b2c4f451cd
NeoJax <NeoJax@gmail.com>
2017-02-20 17:29:48 -0500

    Final Final Copy
    I have managed to fix the fuck-up i have made also am dead

--- Deaf-Grandma 2.js
+++ Deaf-Grandma 2.js
@@ -1,18 +1,22 @@
 var words = "";
 
-while (words != "I love ya
-
-function deaf_grandma(words) {
+while (words != "AM DEAD"){
+    words = prompt ("What are you going to say to grandma?");
 	console.log(words);
 	if (words === "I love ya, Grandma, but I've got to go.") {
-		console.log("OKAY, BYE SONNY.")
+		console.log("OKAY, BYE SONNY.");
+		words = prompt("What say grandma");
+		if(words === ""){
+			words = prompt("WHAT GRANDMA");
+			console.log("WHERE U AT MAH BOI");
+			if(words === ""){
+				console.log("AM LOST WHERE U");
+				words = "AM DEAD";
+			}
+		}
 	} else if (words != words.toUpperCase()) {
 		console.log("HUH?! SPEAK UP, SONNY!");
 	} else if (words != words.toLowerCase()) {
 		console.log("NO, NOT SINCE 1983!"); 
 	}
-	};
 };
-
-var words = prompt("What ya want to say to grandma?");
-deaf_grandma(words);
