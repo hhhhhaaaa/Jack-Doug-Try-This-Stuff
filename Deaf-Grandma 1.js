802320122f1edf1b6a18604aff84fc4a8302b095
Doug L <hathemagicpenguin@gmail.com>
2017-02-20 17:26:41 -0500

    Final Copy
    Ran through, perfected script, errything's just fine

--- Deaf-Grandma 1.js
+++ Deaf-Grandma 1.js
@@ -1,19 +1,23 @@
 function deaf_grandma(words) {
-	console.log(words);
+    console.log(words);
 	if (words === "I love ya, Grandma, but I've got to go.") {
-		console.log("OKAY, BYE SONNY.")
-		var finish = prompt("YA GOOD?")
-		if (finish = "") {
-			console.log("You have finished talking to your grandma simulator. Thank you.")
-	} else { break;
-	};
+		console.log("OKAY, BYE SONNY.");
+		var finish = prompt("YA GOOD?");
+		if (finish === "") {
+			var superfinish = prompt("YA REALLY GOOD?");
+			if (superfinish === "") {
+			console.log("You have finished talking to your grandma simulator. Thank you.");
+			}
+			else {deaf_grandma(superfinish);
+			}
+	} else {deaf_grandma(finish);
+	}
 	} else if (words != words.toUpperCase()) {
 		console.log("HUH?! SPEAK UP, SONNY!");
 	} else if (words != words.toLowerCase()) {
 		console.log("NO, NOT SINCE 1983!"); 
 	}
-	};
-};
+}
 
 var words = prompt("What ya want to say to grandma?");
 deaf_grandma(words);
